de6256d395d4eb17fc7a76d7cd3fb33cf53ceaf5
mmgonnar <gn.mariela@gmail.com>
2026-01-23 16:56:34 -0600

    feat: :art: Updated footer icons formatting and order

--- features/footer/utils/constants.ts
+++ features/footer/utils/constants.ts
@@ -1,9 +1,13 @@
-import Github from "../assets/github.svg"
-import Linkedin from "../assets/linkedin.svg"
-import Mail from "../assets/mail.svg"
+import Github from '../assets/github.svg';
+import Linkedin from '../assets/linkedin.svg';
+import Mail from '../assets/mail.svg';
 
-export const footerIcons =[
-    {icon: Github, name: "Github", href: "https://github.com/mmgonnar"},
-    {icon: Linkedin, name: "LinkedIn", href: "https://www.linkedin.com/in/mmgonnar/"},
-    {icon: Mail, name: "Email", href: 'mailto:mm.gonnar@gmail.com'},
-]
+export const footerIcons = [
+  { icon: Mail, name: 'Email', href: 'mailto:mm.gonnar@gmail.com' },
+  {
+    icon: Linkedin,
+    name: 'LinkedIn',
+    href: 'https://www.linkedin.com/in/mmgonnar/',
+  },
+  { icon: Github, name: 'Github', href: 'https://github.com/mmgonnar' },
+];
