65fcec9f4de3a97ce1d9bbeeff6d7e81c02fa695
mmgonnar <gn.mariela@gmail.com>
2026-02-13 15:33:46 -0600

    Commented some carousel functions

--- features/ui/components/carousel.tsx
+++ features/ui/components/carousel.tsx
@@ -182,71 +182,71 @@ function CarouselItem({ className, ...props }: React.ComponentProps<'div'>) {
   );
 }
 
-function CarouselPrevious({
-  className,
-  variant = 'outline',
-  size = 'icon',
-  ...props
-}: React.ComponentProps<typeof Button>) {
-  const { orientation, scrollPrev, canScrollPrev } = useCarousel();
+// function CarouselPrevious({
+//   className,
+//   variant = 'outline',
+//   size = 'icon',
+//   ...props
+// }: React.ComponentProps<typeof Button>) {
+//   const { orientation, scrollPrev, canScrollPrev } = useCarousel();
 
-  return (
-    <button
-      data-slot="carousel-previous"
-      variant={variant}
-      size={size}
-      className={cn(
-        'absolute size-8 rounded-full',
-        orientation === 'horizontal'
-          ? 'top-1/2 -left-12 -translate-y-1/2'
-          : '-top-12 left-1/2 -translate-x-1/2 rotate-90',
-        className
-      )}
-      disabled={!canScrollPrev}
-      onClick={scrollPrev}
-      {...props}
-    >
-      <ArrowLeft />
-      <span className="sr-only">Previous slide</span>
-    </button>
-  );
-}
+//   return (
+//     <button
+//       data-slot="carousel-previous"
+//       variant={variant}
+//       size={size}
+//       className={cn(
+//         'absolute size-8 rounded-full',
+//         orientation === 'horizontal'
+//           ? 'top-1/2 -left-12 -translate-y-1/2'
+//           : '-top-12 left-1/2 -translate-x-1/2 rotate-90',
+//         className
+//       )}
+//       disabled={!canScrollPrev}
+//       onClick={scrollPrev}
+//       {...props}
+//     >
+//       <ArrowLeft />
+//       <span className="sr-only">Previous slide</span>
+//     </button>
+//   );
+// }
 
-function CarouselNext({
-  className,
-  variant = 'outline',
-  size = 'icon',
-  ...props
-}: React.ComponentProps<typeof Button>) {
-  const { orientation, scrollNext, canScrollNext } = useCarousel();
+// function CarouselNext({
+//   className,
+//   variant = 'outline',
+//   size = 'icon',
+//   ...props
+// }: React.ComponentProps<typeof Button>) {
+//   const { orientation, scrollNext, canScrollNext } = useCarousel();
 
-  return (
-    <button
-      data-slot="carousel-next"
-      variant={variant}
-      size={size}
-      className={cn(
-        'absolute size-8 rounded-full',
-        orientation === 'horizontal'
-          ? 'top-1/2 -right-12 -translate-y-1/2'
-          : '-bottom-12 left-1/2 -translate-x-1/2 rotate-90',
-        className
-      )}
-      disabled={!canScrollNext}
-      onClick={scrollNext}
-      {...props}
-    >
-      <ArrowRight />
-      <span className="sr-only">Next slide</span>
-    </button>
-  );
-}
+//   return (
+//     <button
+//       data-slot="carousel-next"
+//       variant={variant}
+//       size={size}
+//       className={cn(
+//         'absolute size-8 rounded-full',
+//         orientation === 'horizontal'
+//           ? 'top-1/2 -right-12 -translate-y-1/2'
+//           : '-bottom-12 left-1/2 -translate-x-1/2 rotate-90',
+//         className
+//       )}
+//       disabled={!canScrollNext}
+//       onClick={scrollNext}
+//       {...props}
+//     >
+//       <ArrowRight />
+//       <span className="sr-only">Next slide</span>
+//     </button>
+//   );
+// }
 
 export {
   type CarouselApi,
   Carousel,
   CarouselContent,
   CarouselItem,
-  CarouselPrevious,
-  CarouselNext,
+  // CarouselPrevious,
+  // CarouselNext,
 };
