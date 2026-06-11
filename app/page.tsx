cfeb1e97c71a6ea15a9ebe1ebf8f7ed64ab221d6
mmgonnar <gn.mariela@gmail.com>
2026-06-10 20:34:30 -0600

    feat: :sparkles: Optimize Projects component loading by implementing dynamic import with SSR disabled, improving initial load performance and user experience.

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
 'use client';
 
+import dynamic from 'next/dynamic';
 import AboutMe from '@/features/about-me/components/about-me';
 import Banner from '@/features/banner/components/banner';
 import ContactMe from '@/features/contact-me/components/contact-me';
 import Layout from '@/features/layouts/layout';
-import Projects from '@/features/projects/componentes/projects';
 import Skills from '@/features/skills/components/skills';
+
+const Projects = dynamic(
+  () => import('@/features/projects/componentes/projects'),
+  { ssr: false },
+);
 
 export default function Home() {
   return (
